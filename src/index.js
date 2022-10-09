24fe89feabd002c952a934cc94d4d9ca404f1b31
buivd4 <buivd4@hotmail.com>
2022-10-10 00:17:14 +0700

    initial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+ReactDOM.render(<h1 class="text-bold text-5xl">Hello world!</h1>, document.getElementById("root"))
